198bf4b53d025d498a06a09fbf81d2e191e4add0
Nisa Nur Gürel <nisa.gurel@sakarya.edu.tr>
2025-12-06 23:08:33 +0300

    Trainer CRUD eklendi, service checkbox yapısı tamamlandı

---
+++ Controllers/TrainersController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FitnessCenterApp.Data;
+using FitnessCenterApp.Models;
+using FitnessCenterApp.Models.ViewModels;
+
+namespace FitnessCenterApp.Controllers
+{
+    public class TrainersController : Controller
+    {
+        private readonly FitnessDbContext _context;
+
+        public TrainersController(FitnessDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trainers
+        public async Task<IActionResult> Index()
+        {
+            var fitnessDbContext = _context.Trainers.Include(t => t.FitnessCenter);
+            return View(await fitnessDbContext.ToListAsync());
+        }
+
+        // GET: Trainers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers
+                .Include(t => t.FitnessCenter)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        // GET: Trainers/Create
+        public IActionResult Create()
+        {
+            ViewData["FitnessCenterId"] = new SelectList(_context.FitnessCenters, "Id", "Address");
+            return View();
+        }
+
+        // POST: Trainers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TrainerViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // ÖNCE Trainer kaydedilir
+                _context.Add(model.Trainer);
+                await _context.SaveChangesAsync();
+
+                // SONRA Many-to-Many Servis bağlantıları eklenir
+                if (model.SelectedServiceIds != null)
+                {
+                    foreach (var serviceId in model.SelectedServiceIds)
+                    {
+                        var service = await _context.Services.FindAsync(serviceId);
+                        model.Trainer.Services.Add(service);
+                    }
+
+                    await _context.SaveChangesAsync();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Form bozulursa dropdownlar geri dolar
+            ViewData["FitnessCenterId"] =
+                new SelectList(_context.FitnessCenters, "Id", "Name", model.Trainer.FitnessCenterId);
+
+            model.Services = _context.Services
+                .Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.ServiceName
+                })
+                .ToList();
+
+            return View(model);
+        }
+
+        // GET: Trainers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers.FindAsync(id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+            ViewData["FitnessCenterId"] = new SelectList(_context.FitnessCenters, "Id", "Address", trainer.FitnessCenterId);
+            return View(trainer);
+        }
+
+        // POST: Trainers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,SpecialtyText,FitnessCenterId")] Trainer trainer)
+        {
+            if (id != trainer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trainer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrainerExists(trainer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FitnessCenterId"] = new SelectList(_context.FitnessCenters, "Id", "Address", trainer.FitnessCenterId);
+            return View(trainer);
+        }
+
+        // GET: Trainers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainer = await _context.Trainers
+                .Include(t => t.FitnessCenter)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainer);
+        }
+
+        // POST: Trainers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trainer = await _context.Trainers.FindAsync(id);
+            if (trainer != null)
+            {
+                _context.Trainers.Remove(trainer);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrainerExists(int id)
+        {
+            return _context.Trainers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/ViewModels/TrainerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FitnessCenterApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FitnessCenterApp.Models.ViewModels
+{
+    public class TrainerViewModel
+    {
+        public Trainer Trainer { get; set; }
+
+        public List<SelectListItem> Services { get; set; }
+
+        // Checkbox ile seçilecek olan Service Id listesi
+        public List<int> SelectedServiceIds { get; set; }
+    }
+}
